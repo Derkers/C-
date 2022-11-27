59198fc1f6090e2aac958c60e35ed881c695c087
Yury Pidenko <kenjizz@mail.ru>
2022-11-27 17:48:29 +0300

    Example 005

---
+++ Exampele001_HelloConsole/Ex005/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Ввкдите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша") {
+    Console.WriteLine("Ура это же Маша");
+} else {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
